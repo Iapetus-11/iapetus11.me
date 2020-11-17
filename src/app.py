7fb5ce362d039e197a6bd331470b6017b39d3733
Iapetus-11 <olimdahacker@gmail.com>
2020-11-16 22:44:06 -0500

    load home page + comments

--- src/app.py
+++ src/app.py
@@ -4,15 +4,17 @@ import asyncio
 import jinja2
 
 # Routes
-import routes.example as EXAMPLE_ROUTE
+# import routes.example as route_example
+import routes.index as route_index
 
 async def main():
     app = aiohttp.web.Application(client_max_size=67108864)
 
     # Load / use routes
-    # app.add_routes(EXAMPLE_ROUTE.router)
+    # app.add_routes(route_example.router)
+    app.add_routes(route_index.router)
 
-    aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader('src/pages'))  # load templates/pages
+    aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader('pages'))  # load templates/pages
 
     return app
 
