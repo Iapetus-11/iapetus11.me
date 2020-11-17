8e0f05a175b83d5d0efd1c6ee673a09f98a04a98
Iapetus-11 <olimdahacker@gmail.com>
2020-11-16 22:44:17 -0500

    remove unecessary code

--- src/routes/index.py
+++ src/routes/index.py
@@ -5,9 +5,5 @@ router = aiohttp.web.RouteTableDef()
 
 @router.get('/')
 @aiohttp_jinja2.template('index.html')
-async def index(r: aiohttp.web.Request):
-    return {}
-
-
 async def index(req: aiohttp.web.Request):
-    return aiohttp.web.Response(status=200)
+    return {}
