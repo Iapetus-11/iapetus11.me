5400410570b71cc1bc14287e8a4a555d98e1f551
Iapetus-11 <olimdahacker@gmail.com>
2020-11-16 22:07:31 -0500

    add example route

---
+++ src/routes/example.py
@@ -0,0 +1,11 @@
+import aiohttp
+import asyncio
+
+router = aiohttp.web.RouteTableDef()
+
+@router.get('/')
+async def hello(req: aiohttp.web.Request):
+    return aiohttp.web.Response(status=200)
+
+def setup(app)
+    app.add_routes(router)
