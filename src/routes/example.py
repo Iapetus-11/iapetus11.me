985141c66cdcb7d3090d7483336006e99075815b
Iapetus-11 <olimdahacker@gmail.com>
2020-11-16 22:36:22 -0500

    remove setup function from example.py

--- src/routes/example.py
+++ src/routes/example.py
@@ -6,6 +6,3 @@ router = aiohttp.web.RouteTableDef()
 @router.get('/')
 async def hello(req: aiohttp.web.Request):
     return aiohttp.web.Response(status=200)
-
-def setup(app):
-    app.add_routes(router)
