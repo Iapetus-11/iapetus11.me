bbd53ec7d04830a8538a7cc65ca0910902cfe1e7
Iapetus-11 <olimdahacker@gmail.com>
2020-11-16 22:27:02 -0500

    revert

--- src/app.py
+++ src/app.py
@@ -3,16 +3,12 @@ import aiohttp
 import asyncio
 import jinja2
 
-from routes import ROUTERS
-
 async def main():
-    cd = os.getcwd()
     app = aiohttp.web.Application(client_max_size=67108864)
 
-    for router in ROUTERS: app.add_routes(router)
+    app.add_routes(router)
 
     aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader('src/pages'))  # load templates/pages
-
     aiohttp.web.run_app(app)
 
 if __name__ == '__main__':
--- src/routes/__index__.py
+++
@@ -1,7 +0,0 @@
-import os
-
-ROUTERS = []
-
-for route_file in os.listdir():
-    if route_file.endswith('.py') and route_file != 'example.py':
-        ROUTERS.append(__import__(route_file.replace('.py', '')))
