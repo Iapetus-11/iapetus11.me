8ee9301611cfb8d293d85248370c5f474deae587
Iapetus-11 <olimdahacker@gmail.com>
2020-11-25 10:22:07 -0500

    update route names

--- src/routes/error.py
+++ src/routes/error.py
@@ -3,7 +3,7 @@ import aiohttp
 
 router = aiohttp.web.RouteTableDef()
 
-@router.get('/')
+@router.get('/500')
 @aiohttp_jinja2.template('500.html')
 async def index(req: aiohttp.web.Request):
     return {}
--- src/routes/notfound.py
+++ src/routes/notfound.py
@@ -3,7 +3,7 @@ import aiohttp
 
 router = aiohttp.web.RouteTableDef()
 
-@router.get('/')
+@router.get('/404')
 @aiohttp_jinja2.template('404.html')
 async def index(req: aiohttp.web.Request):
     return {}
