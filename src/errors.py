20744ce68b2692addb2dba27153d7221ef72e034
Milo Weinberg <iapetus011@gmail.com>
2021-01-16 11:38:57 -0500

    remove unecessary elif after raise

--- src/errors.py
+++ src/errors.py
@@ -11,8 +11,8 @@ def add_error_handler(app):
         except Exception as e:
             if isinstance(e, aiohttp.web.HTTPNotFound):
                 raise aiohttp.web.HTTPFound('/404')
-            else:
-                print(''.join(traceback.format_exception(type(e), e, e.__traceback__, 4)))
-                raise aiohttp.web.HTTPFound('/500')
+
+            print(''.join(traceback.format_exception(type(e), e, e.__traceback__, 4)))
+            raise aiohttp.web.HTTPFound('/500')
 
     app.middlewares.append(error_handler)
