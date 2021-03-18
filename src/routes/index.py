f9c5f38fe281562fcbfd3e656cfa293eeed16e1d
Milo Weinberg <iapetus011@gmail.com>
2021-03-18 08:34:08 -0400

    goodbye python

--- src/routes/index.py
+++
@@ -1,9 +0,0 @@
-import aiohttp_jinja2
-import aiohttp
-
-router = aiohttp.web.RouteTableDef()
-
-@router.get('/')
-@aiohttp_jinja2.template('index.html')
-async def index(req: aiohttp.web.Request):
-    return {}
