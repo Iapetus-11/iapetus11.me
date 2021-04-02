1affcadc851d4e576e1a7c5b1eb4e36f7bbed07b
Milo Weinberg <iapetus011@gmail.com>
2021-04-02 15:19:12 -0400

    work on rewrite

--- src/util/asciifier.nim
+++ src/util/asciifier.nim
@@ -1,34 +1,18 @@
-import streams
-import nimPNG
+import imageman
 
-proc calculateLuminosity(r: uint8, g: uint8, b: uint8, a: uint8): float =
-  return (0.2126 * float(r) + 0.7152 * float(g) + 0.0722 * float(b)) * (255.0 / float(a))
 
-proc getAsciiValue(palette: string, luminosity: float): char =
-  return palette[int((luminosity / 255.0) * float(palette.len - 1))]
 
-proc asciify(image: PNGResult, palette: string): StringStream =
-  let size = image.width * image.height
-  let imageData = newStringStream(image.data)  # four bytes per pixel
+proc asciify(image: Image, palette: string): string =
+  for y in countup(0, image.height - 1):
+    for x in countup(0, image.width - 1):
+      let r, g, b =
+      let lumi = 0.2126 * r + 0.7152 * g + 0.0722 * b
+      result &= palette.getAsciiValue(calculateLuminosity(image[x, y]))
 
-  result = newStringStream()
-
-  for i in 0..size-1:
-    let r = imageData.readUint8()
-    let g = imageData.readUint8()
-    let b = imageData.readUint8()
-    let a = imageData.readUint8()
-
-    let luminosity = calculateLuminosity(r, g, b, a)
-    result.write(palette.getAsciiValue(luminosity))
-
-    if i mod image.width == image.width - 1:
-      result.write('\n')
+    result &= '\n'
 
 when isMainModule:
-  let image = loadPNG32("src/static/images/petus.png")
+  let image = loadImage[ColorRGBU]("src/static/images/petus.png")
+  let asciiImage = asciify(image, " `-~+#@")
 
-  let asciiTextStream = asciify(image, " `-~+#@")
-  asciiTextStream.setPosition(0)
-
-  echo asciiTextStream.readAll()
+  echo asciiImage
