544ca96a7342b649b9d573cd2184b69ad7a9e2f8
Milo Weinberg <iapetus011@gmail.com>
2021-04-02 23:15:40 -0400

    work on asciifier

--- src/util/asciifier.nim
+++ src/util/asciifier.nim
@@ -1,18 +1,23 @@
 import imageman
 
-
-
 proc asciify(image: Image, palette: string): string =
   for y in countup(0, image.height - 1):
     for x in countup(0, image.width - 1):
-      let r, g, b =
-      let lumi = 0.2126 * r + 0.7152 * g + 0.0722 * b
-      result &= palette.getAsciiValue(calculateLuminosity(image[x, y]))
+      let
+        r = image[x, y].r.float
+        g = image[x, y].g.float
+        b = image[x, y].b.float
+        lumi = 0.2126 * r + 0.7152 * g + 0.0722 * b
+        index = lumi * (palette.len - 1).float
+
+      echo lumi, ", ", index
+
+      result &= palette[index.int]
 
     result &= '\n'
 
 when isMainModule:
-  let image = loadImage[ColorRGBU]("src/static/images/petus.png")
+  let image = loadImage[ColorRGBF]("src/static/images/petus.png")
   let asciiImage = asciify(image, " `-~+#@")
 
   echo asciiImage
