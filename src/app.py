83b01117c8c9d6b015e3b50a8931bbb75d47f19e
Milo Weinberg <iapetus011@gmail.com>
2021-06-17 11:43:18 -0400

    add base app.py

---
+++ src/app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+import traceback
+
+app = Flask(__name__, static_url_path="/static", static_folder="static")
+
+@app.route("/")
+def page_root():
+    return app.send_static_file("pages/index.html")
+
+@app.route("/projects")
+def page_projects():
+    return app.send_static_file("pages/projects.html")
+
+@app.errorhandler(404)
+def page_404(e):
+    return app.send_static_file("pages/404.html")
+
+@app.errorhandler(500)
+def page_500(e):
+    print("".join(traceback.format_exception(type(e), e, e.__traceback__, 4)))
+    return app.send_static_file("pages/500.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
