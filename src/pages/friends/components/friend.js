5f8bdf84461bb859539c5ddc49c23c56833a10d9
Milo Weinberg <iapetus011@gmail.com>
2022-07-11 02:17:42 -0400

    fix build error

--- src/pages/friends/components/friend.js
+++ src/pages/friends/components/friend.js
@@ -2,7 +2,7 @@ import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 
 export default function Friend({ name, username, url, content, idx }) {
   return (
-    <div className={`${content.length <= 160 ? "md:w-1/2" : "mx-auto"} md:px-8 hover:opacity-90`}>
+    <div className={`${(content?.length ?? 0) <= 160 ? "md:w-1/2" : "mx-auto"} md:px-8 hover:opacity-90`}>
       <a href={url} target="_blank" rel="noreferrer">
         <div className={`fin fin-${idx} flex flex-row p-8 bg-black bg-opacity-30 rounded-3xl`}>
           <span className="text-white text-lg">
