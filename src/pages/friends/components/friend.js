4bc5986cd316b881128772e88cd38a6a10ceadd0
Iapetus-11 <Iapetus-11@users.noreply.github.com>
2022-07-12 01:45:40 +0000

    Prettified Code!

--- src/pages/friends/components/friend.js
+++ src/pages/friends/components/friend.js
@@ -3,12 +3,12 @@ import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 export default function Friend({ name, username, url, content, idx }) {
   return (
     <div
-      className={`${
-        (content?.length ?? 0) <= 160 ? "md:w-1/2" : "mx-auto"
-      } md:px-8 self-center`}
+      className={`${(content?.length ?? 0) <= 160 ? "md:w-1/2" : "mx-auto"} md:px-8 self-center`}
     >
       <a href={url} target="_blank" rel="noreferrer">
-        <div className={`fin fin-${idx} flex flex-row p-8 bg-black transition bg-opacity-30 hover:bg-opacity-20 rounded-2xl`}>
+        <div
+          className={`fin fin-${idx} flex flex-row p-8 bg-black transition bg-opacity-30 hover:bg-opacity-20 rounded-2xl`}
+        >
           <span className="text-white text-lg">
             {/* eslint-disable-next-line @next/next/no-img-element */}
             <img
@@ -20,9 +20,7 @@ export default function Friend({ name, username, url, content, idx }) {
             <span className="text-aqua-normal text-3xl">{name}</span>
             <br />
 
-            <ReactMarkdown linkTarget="_blank">
-              {content}
-            </ReactMarkdown>
+            <ReactMarkdown linkTarget="_blank">{content}</ReactMarkdown>
           </span>
         </div>
       </a>
