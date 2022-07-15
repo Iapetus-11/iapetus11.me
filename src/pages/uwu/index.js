454d31fd228f3e815d92c8f71af3cdada00f94ee
Milo Weinberg <iapetus011@gmail.com>
2022-07-14 23:44:12 -0400

    uwu

---
+++ src/pages/uwu/index.js
@@ -0,0 +1,29 @@
+import Default from "../../ui/default";
+
+export default function Uwu() {
+  return (
+    <Default>
+      <div className="flex flex-col w-full h-full justify-center text-center">
+        <span className="text-9xl text-center">🥺</span>
+
+        <div className="flex flex-row w-full justify-center">
+          <div className="-rotate-90">
+            <div className="animate-bounce">
+              <div className="rotate-90">
+                <span className="text-8xl">👉</span>
+              </div>
+            </div>
+          </div>
+
+          <div className="rotate-90">
+            <div className="animate-bounce">
+              <div className="-rotate-90">
+                <span className="text-8xl">👈</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Default>
+  )
+}
