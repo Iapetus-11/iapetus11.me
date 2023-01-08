df2b8bf5befbfb1826776094bd644e7aa48faa6c
Milo Weinberg <iapetus011@gmail.com>
2023-01-08 15:37:25 -0500

    Remove testing code

--- bruh.js
+++
@@ -1,8 +0,0 @@
-function calculateYearsElapsed(since) {
-    return new Date(new Date().getTime() - since.getTime()).getFullYear() - 1970;
-}
-
-const aliveForYears = calculateYearsElapsed(new Date('August, 2003'));
-const programmingForYears = calculateYearsElapsed(new Date('August, 2016'));
-
-console.log(aliveForYears, programmingForYears);
