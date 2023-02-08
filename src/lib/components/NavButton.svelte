c8029c576abd63262e3205cb3bdea2f0d595db4c
Milo Weinberg <iapetus011@gmail.com>
2023-02-08 12:18:40 -0500

    Simplify NavButton classes

--- src/lib/components/NavButton.svelte
+++ src/lib/components/NavButton.svelte
@@ -5,8 +5,8 @@
 </script>
 
 <a
-    class="flex fin py-2 px-2 sm:px-4 hover:bg-aqua-dark transform duration-100 sm:hover:scale-110 border
-           hover:border-opacity-0 border-white rounded-md h-8 sm:h-10 justify-center {$$props.class}"
+    class="flex fin py-2 px-2 sm:px-4 hover:bg-aqua-dark border border-white
+           hover:border-opacity-0 rounded-md h-8 sm:h-10 justify-center {$$props.class}"
     style="animation-delay:{0.2 + idx * 0.05}s;"
     {href}
 >
