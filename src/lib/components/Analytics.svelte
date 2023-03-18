84b79b35edc19bc4850311e6ccb5ba00afbc1b0d
Milo Weinberg <iapetus011@gmail.com>
2023-03-18 08:54:48 -0400

    Remove google analytics

--- src/lib/components/Analytics.svelte
+++ src/lib/components/Analytics.svelte
@@ -1,28 +1,7 @@
-<script lang="ts">
-    import { PUBLIC_GTAG_ID, PUBLIC_UNAMI_ID } from '$env/static/public';
-    import { page } from '$app/stores'
-
-    $: {
-        if (typeof gtag !== 'undefined') {
-            gtag('config', PUBLIC_GTAG_ID, {
-                page_title: document.title,
-                page_path: $page.url.pathname,
-            });
-        }
-    }
+<script lang='ts'>
+    import { PUBLIC_UNAMI_ID } from '$env/static/public';
 </script>
 
 <svelte:head>
-    {@html `<script async defer src="https://analytics.umami.is/script.js" data-website-id="${PUBLIC_UNAMI_ID}" />`}
-    {@html `<script async src="https://www.googletagmanager.com/gtag/js?id=${PUBLIC_GTAG_ID}" />` }
-    {@html `<script>
-        window.dataLayer = window.dataLayer || [];
-
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-
-        gtag('js', new Date());
-        gtag('config', '${PUBLIC_GTAG_ID}');
-    </script>`}
+    {@html `<script async defer src='https://analytics.umami.is/script.js' data-website-id='${PUBLIC_UNAMI_ID}' />`}
 </svelte:head>
