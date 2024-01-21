858169075b3cfa8fdb81862106306a1192f665ba
Milo Weinberg <iapetus011@gmail.com>
2024-01-21 09:32:09 -0500

    Remove old SvelteKit site

--- src/lib/components/Analytics.svelte
+++
@@ -1,7 +0,0 @@
-<script lang='ts'>
-    import { PUBLIC_UNAMI_ID } from '$env/static/public';
-</script>
-
-<svelte:head>
-    {@html `<script async defer data-website-id="${PUBLIC_UNAMI_ID}" src="https://analytics.umami.is/script.js"></script>`}
-</svelte:head>
--- src/routes/friends/+page.ts
+++
@@ -1,9 +0,0 @@
-import { getRecommendations } from '$lib/api/github/repo';
-
-export const ssr = false;
-
-export async function load() {
-    return {
-        recommendations: (await getRecommendations()).reverse(),
-    };
-}
