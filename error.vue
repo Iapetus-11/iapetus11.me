07f759ae95ef40ac01c4ceb794548b54e6ef51b7
Milo Weinberg <iapetus011@gmail.com>
2024-11-07 20:33:58 -0500

    Start on error page

---
+++ error.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+    import type { NuxtError } from '#app';
+
+
+    const props = defineProps<{
+        error: NuxtError;
+    }>();
+
+    const title = computed<string>(() => ({
+        404: 'Page Not Found',
+    }[props.error.statusCode] ?? 'Error'));
+
+    useHead({
+        title: 'Title',
+        bodyAttrs: {
+            class:
+                'select-none bg-dark-dark bg-gradient-to-r from-dark-dark via-dark-light via-dark-normal ' +
+                'to-dark-dark',
+        },
+    });
+</script>
+
+<template>
+    <code class="text-white font-mono">
+        {{ error }}
+    </code>
+</template>
