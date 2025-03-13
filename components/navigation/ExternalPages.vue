c129b182d9b254078b1d031d9aeffb2f07edbd20
Milo Weinberg <iapetus011@gmail.com>
2025-03-12 20:43:20 -0400

    Start on fixing discord button

--- components/navigation/ExternalPages.vue
+++ components/navigation/ExternalPages.vue
@@ -5,27 +5,33 @@
 </script>
 
 <script setup lang="ts">
-    import { faDiscord, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+    import { faDiscord, faGithub, faLinkedin, type IconDefinition } from '@fortawesome/free-brands-svg-icons';
     import { faDonate } from '@fortawesome/free-solid-svg-icons';
     import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+    const DISCORD_USERNAME = 'iapetus11';
 
     defineProps<{
         animationDelay?: number;
     }>();
+    
+    function showAndCopyDiscordUsername() {
+        navigator.clipboard.writeText(DISCORD_USERNAME);
+        setTimeout(() => discordUsernameModalOpen.value = false, 1000);
+    }
 
     const EXTERNAL_PAGES = [
         { href: 'https://github.com/Iapetus-11', icon: faGithub },
         { href: 'https://www.linkedin.com/in/milo-weinberg', icon: faLinkedin },
-        { href: 'https://dsc.bio/iapetus11', icon: faDiscord },
+        { action: () => {}, icon: faDiscord },
         { href: 'https://www.venmo.com/u/Iapetus11', icon: faDonate },
-    ] as const;
+    ] as const satisfies (({ action: () => void } | { href: string }) & { icon: IconDefinition })[];
 </script>
 
 <template>
     <NuxtLink
         v-for="(page, idx) in EXTERNAL_PAGES"
         :key="page.href"
-        npm
         :href="page.href"
         class="animate-fade-in cursor-pointer text-2xl text-white transition-all hover:text-aqua-normal"
         :class="$attrs.class"
@@ -35,4 +41,8 @@
     >
         <FontAwesomeIcon :icon="page.icon" :href="page.href" />
     </NuxtLink>
+
+    <div>
+
+    </div>
 </template>
