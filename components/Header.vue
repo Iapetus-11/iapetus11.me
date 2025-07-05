4d5fc2410ca68968f069a187919c81abca303415
Milo Weinberg <iapetus011@gmail.com>
2025-07-05 12:50:52 -0400

    Changes I can't remember

--- components/Header.vue
+++ components/Header.vue
@@ -49,6 +49,16 @@
         ref="header"
         class="bg-dull/75 shadow-dull sticky top-0 z-30 mb-13 flex h-18 w-full items-center p-6 px-14 opacity-0 shadow-lg"
     >
+        <div
+            style="filter: blur(3px)"
+            class="flex h-full w-full overflow-hidden absolute *:w-[200px] *:h-[45px] *:rotate-[-45deg] *:absolute *:top-0 *:opacity-100 transition-opacity duration-100"
+            :class="isPastScrollYThreshold ? 'opacity-100' : 'opacity-0'"
+        >
+            <div class="bg-gradient-to-r from-white/40 to-white mr-20 !opacity-5"></div>
+            <div class="bg-gradient-to-r from-white/40 to-dull mr-70 !h-[20px]"></div>
+            <div class="bg-gradient-to-r from-white/40 to-gray mr-40 !h-[90px] !w-[250px]"></div>
+        </div>
+
         <img
             src="~/assets/images/petus.png"
             alt="Iapetus11's Profile Picture"
@@ -60,7 +70,7 @@
             {{ $route.meta.title }}
         </h1>
 
-        <nav class="ml-auto flex gap-2">brh</nav>
+        <nav class="ml-auto flex gap-2">brh {{  windowScroll }} -> {{ isPastScrollYThreshold}}</nav>
 
         <div ref="header-line" class="bg-glow glow-purple absolute top-0 left-0 h-[1px]"></div>
     </header>
