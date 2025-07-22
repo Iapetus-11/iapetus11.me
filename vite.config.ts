e27f563308a8b209ed7a10fcf24e87acd91d95ea
Milo Weinberg <iapetus011@gmail.com>
2025-07-22 18:14:09 -0400

    Configure vite-ssg to minify index.html

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,19 @@
 import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite';
+import { UserConfig } from 'vite';
+import { type ViteSSGOptions } from 'vite-ssg';
 import vue from '@vitejs/plugin-vue';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
-export default defineConfig({
+export default {
     plugins: [vue(), tailwindcss()],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
         },
     },
-});
+    ssgOptions: {
+        formatting: 'minify',
+    },
+} satisfies UserConfig & { ssgOptions: ViteSSGOptions };
