872135571032687fdbc9c88f74998f8f56dc8783
Milo Weinberg <iapetus011@gmail.com>
2025-07-23 20:15:09 -0400

    Increase max width of layout

--- src/components/layout/DefaultLayout.vue
+++ src/components/layout/DefaultLayout.vue
@@ -6,7 +6,7 @@
 
 <template>
     <div
-        class="mx-auto flex min-h-screen max-w-[50rem] flex-col items-center overflow-x-clip px-4 py-8 sm:px-8 md:py-10 lg:max-w-[90rem] lg:px-10 xl:py-16 2xl:py-22"
+        class="mx-auto flex min-h-screen max-w-[50rem] flex-col items-center overflow-x-clip px-4 py-8 sm:px-8 md:py-10 lg:max-w-[95rem] lg:px-10 xl:py-16 2xl:py-22"
     >
         <div v-bind="$attrs">
             <slot />
