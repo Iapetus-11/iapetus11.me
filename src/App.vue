6c77a505dd6491d3c001fbf1613ff25fe36ea1ca
Milo Weinberg <iapetus011@gmail.com>
2025-08-05 12:39:46 -0400

    Fix background compatibility issue with Safari

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
     import { RouterView } from 'vue-router';
     import MouseSpotlight from './components/MouseSpotlight.vue';
+    import Background from './components/layout/Background.vue';
 </script>
 
 <template>
+    <Background />
     <MouseSpotlight />
     <RouterView />
 </template>
---
+++ src/components/layout/Background.vue
@@ -0,0 +1,10 @@
+<!--
+    Safari (and some other browsers like Ladybird) do not support fixed gradient backgrounds on the <body> element
+    well, the recommended fix is to just position a div as the background. This solution avoids flickering and other
+    glitchy undesired behavior.
+-->
+<template>
+    <div
+        class="fixed top-0 left-0 -z-50 h-screen w-screen bg-gradient-to-bl from-[#15224F] via-[#040917] to-[#14204A]"
+    ></div>
+</template>
