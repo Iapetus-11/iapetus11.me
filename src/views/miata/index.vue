1a1c641f07cd201c21ea5f25ff5bb8e7f9ad1298
Milo Weinberg <iapetus011@gmail.com>
2025-08-28 20:08:04 -0400

    Miata

--- src/views/miata/index.vue
+++ src/views/miata/index.vue
@@ -1,13 +1,27 @@
 <script setup lang="ts">
-import PageLayout from '@/components/layout/PageLayout.vue';
-
-
+    import PageLayout from '@/components/layout/PageLayout.vue';
 </script>
+
+<style scoped>
+    @reference '@/assets/main.css';
+
+    img {
+        @apply rounded-xl shadow-lg max-sm:translate-x-0 max-sm:translate-y-0 max-sm:scale-100;
+    }
+</style>
 
 <template>
     <PageLayout title="Miata">
-        <div>
-            
+        <div class="grid grid-cols-3">
+            <div class="border-primary-500 border-2 bg-teal-300/5 p-3 rounded-xl">
+                <p>
+                    I own a 2023 ND Miata! I have lightly modified it, not much yet, but with bigger plans for the future.
+                </p>
+            </div>
+
+            <div class="col-span-2">
+                ...
+            </div>
         </div>
     </PageLayout>
 </template>
