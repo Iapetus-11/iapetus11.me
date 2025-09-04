a609c34a8fe75967c0e11d316e7131f3dcebc841
Milo Weinberg <iapetus011@gmail.com>
2025-09-04 19:14:57 -0400

    Make social icons more grid-like on tablet/smaller sized screens

--- src/views/home/social/SocialButton.vue
+++ src/views/home/social/SocialButton.vue
@@ -6,7 +6,7 @@
     }>();
 
     const CLASSES =
-        'rounded-full p-1 text-slate-300 transition-[colors,scale] hover:text-purple-300 text-2xl scale-110 hover:scale-120';
+        'rounded-full max-xs:p-1 md:p-1 text-slate-300 transition-[colors,scale] hover:text-purple-300 text-2xl scale-110 hover:scale-120 xs:max-md:flex xs:max-md:grow xs:max-md:items-center xs:max-md:justify-center xs:max-md:size-[33px]';
 </script>
 
 <template>
