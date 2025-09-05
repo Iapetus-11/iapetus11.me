290384c6528035817ae9a82e32bb326f7e1ee280
Milo Weinberg <iapetus011@gmail.com>
2025-09-05 12:29:43 -0400

    Reorganize homepage components + improve social icons + adjust pfp sizing on smaller layouts

---
+++ src/views/home/nav/desktop/NavItems.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+    import SectionNavLink from './SectionNavLink.vue';
+</script>
+
+<template>
+    <div class="flex flex-col gap-2 2xl:gap-3">
+        <SectionNavLink sttf-id="pictures" icon="icon-[hugeicons--image-02]">
+            Pictures
+        </SectionNavLink>
+        <SectionNavLink sttf-id="projects" icon="icon-[hugeicons--test-tube-01]">
+            Projects
+        </SectionNavLink>
+        <SectionNavLink sttf-id="resume" icon="icon-[hugeicons--ai-content-generator-01]">
+            Experience
+        </SectionNavLink>
+    </div>
+</template>
