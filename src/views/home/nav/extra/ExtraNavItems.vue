f6b539050c8909ae38f3842d581e0422e2f1a7ab
Milo Weinberg <iapetus011@gmail.com>
2025-09-11 07:33:16 -0400

    Start on extra nav items

---
+++ src/views/home/nav/extra/ExtraNavItems.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+    import { computed, useTemplateRef } from 'vue';
+    import NavLink from './NavLink.vue';
+    import { useScrollCardEffect } from '@/utils/scrollCardEffect';
+
+    const container = useTemplateRef('container');
+    const extraNavElements = computed(() => [
+        ...((container.value?.querySelectorAll('div,h2').values() ?? []) as HTMLElement[]),
+    ]);
+
+    useScrollCardEffect(extraNavElements);
+</script>
+
+<template>
+    <div ref="container">
+        <h2 class="mb-3 font-mono text-2xl font-bold whitespace-nowrap max-md:text-center">
+            More&hellip;
+        </h2>
+
+        <div class="grid grid-cols-4 gap-4">
+            <NavLink :to="{ name: 'links' }">
+                Link Dump
+
+                <template #icon>
+                    <span class="icon-[hugeicons--link-03] group-hover:animate-spin"></span>
+                </template>
+            </NavLink>
+
+            <NavLink :to="{ name: 'miata' }">
+                Miata
+
+                <template #icon>
+                    <span class="icon-[hugeicons--car-01]"></span>
+                    <span
+                        class="icon-[hugeicons--fast-wind] -mr-[1em] scale-80 opacity-0 transition-[color,opacity,margin,scale] group-hover:-mr-0 group-hover:scale-100 group-hover:opacity-100"
+                    ></span>
+                </template>
+            </NavLink>
+        </div>
+    </div>
+</template>
---
+++ src/views/home/nav/extra/NavLink.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+    import Link from '@/components/Link.vue';
+</script>
+
+<template>
+    <Link
+        :to="{ name: 'links' }"
+        class="group outlined-actionable flex items-center justify-between overflow-clip rounded-xl p-2 px-2.5 text-lg font-medium 2xl:text-xl"
+    >
+        <slot />
+
+        <div class="flex items-center text-gray-300 transition-colors group-hover:text-white">
+            <slot name="icon" />
+        </div>
+    </Link>
+</template>
