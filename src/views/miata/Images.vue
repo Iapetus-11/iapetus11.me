e786f34fd4fb5c42fe2f9376e96b35f77403db58
Milo Weinberg <iapetus011@gmail.com>
2025-09-12 08:22:51 -0400

    Move some Miata images around

--- src/views/miata/Images.vue
+++ src/views/miata/Images.vue
@@ -1,3 +1,8 @@
 <template>
-    <img src="@/assets/images/me/parking-lot-sunset.webp" alt="Miata in parking lot at sunset" />
+    <img src="@/assets/images/miata/parking-lot-sunset.webp" alt="Miata in parking lot at sunset" />
+    <img src="@/assets/images/miata/cliff-view-w-trinity.webp" alt="Trinity & Miata on a cliff" />
+    <img
+        src="@/assets/images/miata/elk-mountain-graffiti-rd-angled.webp"
+        alt="Miata at Elk Mountain Scenic Highway"
+    />
 </template>
