499877ceef56654ce4c0fbd637473ba7a00861b1
Milo Weinberg <iapetus011@gmail.com>
2025-12-03 19:42:28 -0500

    Prevent duplicate img component + improve mobile design

--- src/views/home/sections/Project.vue
+++ src/views/home/sections/Project.vue
@@ -14,41 +14,38 @@
     <Link
         :to="link"
         :target="link.startsWith('/') ? '_self' : '_blank'"
-        class="group outlined-actionable flex shrink items-center space-x-3 rounded-xl p-4 lg:p-5"
+        class="group outlined-actionable relative flex min-h-43 rounded-xl p-4 lg:p-5"
     >
-        <div class="mr-2 flex h-full w-full flex-col md:mr-5 xl:mr-10">
+        <div class="self-center sm:max-w-[calc(100%-10rem)]">
             <div
-                class="align-self-end mb-1 flex items-center font-mono text-xl font-semibold whitespace-nowrap text-white md:text-2xl"
+                class="mb-1 flex items-center font-mono text-xl font-semibold whitespace-nowrap text-white md:text-2xl"
             >
                 <h3>{{ name }}</h3>
 
                 <span
                     class="icon-[hugeicons--link-square-02] ml-2.5 scale-0 text-purple-300 opacity-0 transition-[opacity,scale] group-hover:scale-110 group-hover:opacity-100"
                 ></span>
-
-                <div class="-mt-2 -mr-4 ml-auto size-10 overflow-hidden rounded-full sm:hidden">
-                    <img
-                        :src="image"
-                        :alt="name"
-                        :loading="imgLoading"
-                        :fetchpriority="imgLoading === 'eager' ? 'high' : 'auto'"
-                    />
-                </div>
             </div>
 
             <p class="text-white">
+                <!-- Make room for the project picture on mobile -->
+                <span class="float-right block h-9 w-16 sm:hidden"></span>
+
                 {{ description }}
             </p>
 
             <BadgeList :badges="points" class="mt-3" />
         </div>
 
-        <div class="ml-auto w-1/3 max-w-[8rem] overflow-hidden rounded-3xl max-sm:hidden">
+        <div
+            class="absolute top-3.5 right-3.5 flex aspect-square items-center justify-end max-sm:h-14 sm:top-1/2 sm:right-5 sm:h-[calc(100%-2rem)] sm:-translate-y-1/2"
+        >
             <img
                 :src="image"
                 :alt="name"
                 :loading="imgLoading"
                 :fetchpriority="imgLoading === 'eager' ? 'high' : 'auto'"
+                class="max-h-[128px] rounded-2xl sm:rounded-3xl"
             />
         </div>
     </Link>
