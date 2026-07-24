25346469217f238522c967f21682b2151b7b1bab
Milo Weinberg <git@iapetus11.me>
2026-07-24 07:48:27 -0400

    Add missing alt attribute

--- src/components/layout/Background.vue
+++ src/components/layout/Background.vue
@@ -30,6 +30,7 @@
                 class="h-screen w-screen object-cover opacity-[0.0001] transition-opacity duration-750"
                 fetchpriority="low"
                 onload="event.target.classList.add('!opacity-[6%]', 'md:opacity-5')"
+                alt=""
             />
         </picture>
     </div>
